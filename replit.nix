c004eec1134eaa3561dc0d561547e210caba248a
Adriellison Ferreira <50798465+adriellison@users.noreply.github.com>
2022-11-08 18:01:58 -0300

    add node 16

--- replit.nix
+++ replit.nix
@@ -1,6 +1,6 @@
 { pkgs }: {
   deps = [
-    pkgs.sudo apt install node
+    pkgs.sudo apt install nodejs-16_x
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server
   ];
